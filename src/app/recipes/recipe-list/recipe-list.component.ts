450ac5b3cc8c1eaa32c0eb63464dd9604e07fc54
renwickp <peter2renwick@gmail.com>
2020-05-02 12:02:26 -0600

    CSS Bootstrap3 V 4

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -10,7 +10,7 @@ export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
     new Recipe(
       "Test Recipe",
-      "Hammer Time",
+      "Recipe Time",
       "https://images.unsplash.com/photo-1515449440149-4396ddd9218b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
     ),
   ];
