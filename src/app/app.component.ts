447aab040935ce2b4893050817b4585ab979dc95
renwickp <peter2renwick@gmail.com>
2020-05-05 22:03:30 -0600

    Nav Bar Broken

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'foodfun';
+  loadedFeature = "recipe";
+
+  title = "foodfun";
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
 }
