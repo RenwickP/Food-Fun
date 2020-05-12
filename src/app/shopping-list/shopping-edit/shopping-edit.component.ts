d8e96668d15e6e679ae49c618b1e108a3ac72de2
renwickp <peter2renwick@gmail.com>
2020-05-11 19:29:58 -0600

    <>

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -14,18 +14,16 @@ import { Ingredient } from "../../shared/ingredient.model";
   styleUrls: ["./shopping-edit.component.css"],
 })
 export class ShoppingEditComponent implements OnInit {
-  @ViewChild("nameInput") nameInputRef: ElementRef;
-  @ViewChild("amountInput") amountInputRef: ElementRef;
-  @Output() ingredientAdded = new EventEmitter<{
-    name: string;
-    amount: number;
-  }>();
+  @ViewChild("nameInput", { static: false }) nameInputRef: ElementRef;
+  @ViewChild("amountInput", { static: false }) amountInputRef: ElementRef;
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
 
   constructor() {}
 
   ngOnInit(): void {}
 
   onAddItem() {
+    debugger;
     const ingName = this.nameInputRef.nativeElement.value;
     const ingAmount = this.amountInputRef.nativeElement.value;
     const newIngredient = new Ingredient(ingName, ingAmount);
