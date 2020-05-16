6f97aaac71dc43fafc6c883bb33db9223bac8f07
renwickp <peter2renwick@gmail.com>
2020-05-16 15:34:11 -0600

    Fixed

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -23,10 +23,10 @@ export class ShoppingEditComponent implements OnInit {
   ngOnInit(): void {}
 
   onAddItem() {
-    debugger;
     const ingName = this.nameInputRef.nativeElement.value;
     const ingAmount = this.amountInputRef.nativeElement.value;
     const newIngredient = new Ingredient(ingName, ingAmount);
     this.ingredientAdded.emit(newIngredient);
+    return false;
   }
 }
