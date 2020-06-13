d812454ce41e5697a8bd4b1edc617a1eebe8532f
renwickp <peter2renwick@gmail.com>
2020-06-13 14:28:36 -0600

    1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
@@ -26,7 +27,7 @@ import { ShoppingListService } from "./shopping-list/shopping-list-service";
     ShoppingEditComponent,
     DropdownDirective,
   ],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
   providers: [ShoppingListService],
   bootstrap: [AppComponent],
 })
